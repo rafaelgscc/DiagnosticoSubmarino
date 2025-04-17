4897df2704b981519f5af39317edc82b47148cb3
rafaelgscc <rafaelgscc@gmail.com>
2025-04-17 14:07:38 -0300

    Adicionando classe CalculaGamaEpisilonStrategy com logica de leitura

--- DiagnosticoSubmarino/Services/CalculaGamaEpisilonStrategy.cs
+++ DiagnosticoSubmarino/Services/CalculaGamaEpisilonStrategy.cs
@@ -1,8 +1,51 @@
 using System;
+using DiagnosticoSubmarino.Interfaces;
+using DiagnosticoSubmarino.Models;
 
 namespace DiagnosticoSubmarino.Services;
 
-public class CalculaGamaEpisilonStrategy
+public class CalculaGamaEpisilonStrategy : IDiagnosticoStrategy
 {
+    public DiagnosticoResultado CalculaTaxas(List<string> relatorio)
+    {
+        int tamanhoBit = relatorio[0].Length;
+        int[] count = new int[tamanhoBit];
 
+        //contando cada bit em cada posicao
+        foreach(var binario in relatorio)
+        {
+            for(int i = 0; i<tamanhoBit; i++)
+            {
+                if(binario[i]==1)
+                    count[i]++;
+            }
+        }
+
+        string gamaString = "", episilonString = "";
+        int metade = relatorio.Count / 2;
+
+        //juncao da string com os valores de Gama e de Epision
+
+        for(int i = 0; i < tamanhoBit; i++)
+        {
+            //fazendo a verificacao das colunas
+            if(count[i]>metade)
+            {
+                gamaString += "1";
+                episilonString += "0";
+            }
+            else
+            {
+                 gamaString += "1";
+                episilonString += "0";
+            }
+        }
+
+        //converter valores para decimal
+
+        int gama = Convert.ToInt32(gamaString, 2);
+        int episilon = Convert.ToInt32(episilonString, 2);
+
+        return new DiagnosticoResultado(gama, episilon);
+    }
 }
--- DiagnosticoSubmarino/obj/Debug/net9.0/DiagnosticoSubmarino.AssemblyInfo.cs
+++ DiagnosticoSubmarino/obj/Debug/net9.0/DiagnosticoSubmarino.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiagnosticoSubmarino")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d54e9f3ceba6c148588ff579cbf2295548d613e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37dcf38901d7fa32297b78ce00d2d66c2ef39407")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiagnosticoSubmarino")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiagnosticoSubmarino")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
